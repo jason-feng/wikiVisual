0152c583aacd8c3245f87ae8e0cabd2eac9ad459
Jason Feng <jason21feng@gmail.com>
2015-05-28 15:07:08 -0400

    get birth year from XML, change output to csv

--- scripts/pageRank.py
+++ scripts/pageRank.py
@@ -1,6 +1,7 @@
 import re
 import extractPage
 import json
+import csv
 from collections import defaultdict
 
 #  File names
@@ -12,31 +13,35 @@ testFile = '../data/listTest'
 listRussianFilmDirectors = [pageId.rstrip('\n') for pageId in open(directorsFile)]
 listRussianFilmDirectors = sorted(listRussianFilmDirectors, key=int)
 
-print listRussianFilmDirectors
-
 pages, pageToTitle = extractPage.process_data(wikiDump,listRussianFilmDirectors)
-
-print 'finished extracting pages'
 
 numLinks = defaultdict(int) #Dictionary of numLinks of each link we find
 pageLinks = defaultdict(list)
 pageRanks = defaultdict(float)
+idBirth = defaultdict(str)
 titlePageRanks = defaultdict(float)
 
-regex='\[\[(.+?)\]\]' # get all matches between [[ ]] this is dump link format
 
+regexLink='\[\[(.+?)\]\]' # get all matches between [[ ]] this is dump link format
+regexBirth='\{\{Birth date(.+?)\}\}' # birth_date = {{Birth date|df=yes|1932|4|4}}
 for idx, directors in enumerate(listRussianFilmDirectors):
-    internal_links = re.findall(regex, pages[idx]) # gets all of the links in a page
+    internal_links = re.findall(regexLink, pages[idx]) # gets all of the links in a page
     internal_links = [link.split('|',1)[0] for link in internal_links] # Gets rid of the name of the link, only keeps the actual link
     for link in internal_links:
         numLinks[link] += 1
         pageLinks[directors].append(link)
+    birth_date = re.findall(regexBirth, pages[idx])[0]
+    birth_date = birth_date.split('|')
+    birth_year = birth_date[2]
+    idBirth[directors] = birth_year
+
+print idBirth
 
 # Initiatize all pageRanks to 1
 for (idx, directors) in enumerate(listRussianFilmDirectors):
     pageRanks[directors] = 1.0;
 #
-# # PageRank of A = 0.15 + 0.85 * PR(B)
+# # PageRank of A = 0.15 + 0.85 * 1/numLinks
 # # Iteration one
 for (idx, directors) in enumerate(listRussianFilmDirectors):
     for link in pageLinks[directors]: # Get list of all links for one page
@@ -51,9 +56,18 @@ for directors in listRussianFilmDirectors:
 
 print titlePageRanks
 
-d = {"name":"Russian Film Directors",
-     "children":[{'name':key,"size":value} for key,value in titlePageRanks.items()]}
-j = json.dumps(d, indent=4)
-f = open('../json/pageRanks.json', 'w')
-print >> f, j
+# Write out data to csv
+f = open('../csv/pageRanks.csv','wb')
+writer = csv.writer(f)
+csv = ["name","value"]
+writer.writerow(csv)
+for key, value in pageRanks.items():
+   writer.writerow([pageToTitle[key], value, idBirth[key], key])
 f.close()
+#
+# d = {"name":"Russian Film Directors",
+#      "children":[{'name':key,"size":value} for key,value in titlePageRanks.items()]}
+# j = json.dumps(d, indent=4)
+# f = open('../json/pageRanks.json', 'w')
+# print >> f, j
+# f.close()
